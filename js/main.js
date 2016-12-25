dd6329cea3776568dabc82b0fdfe96f0225a2fbc
Jeff Wen <sinchang@sinchangs-MacBook-Air.local>
2016-12-25 21:09:49 +0800

    update to v1.2

--- js/main.js
+++ js/main.js
@@ -1,54 +1,56 @@
-$(function() {
-	$(document).ajaxStart(function() {
+$(function () {
+	$(document).ajaxStart(function () {
 		$(".loading").show();
-	}).ajaxStop(function() {
+	}).ajaxStop(function () {
 		$(".loading").hide();
 	});
-	$(".tooltips").hide();
+
 	var clipboard = new Clipboard('.copy');
-	clipboard.on('success', function(e) {
-		$(".tooltips").css("display", "block");
+	clipboard.on('success', function (e) {
+		$(".tooltips").show();
 	});
-	clipboard.on('error', function(e) {
+	clipboard.on('error', function (e) {
 		alert("复制失败");
 	});
-	$('#smfile').each(function() {
-		var $input = $(this),
-			$label = $input.next('label'),
-			labelVal = $label.html();
-		$input.on('change', function(e) {
-			$(".img").hide();
-			if (e.target.value) {
-				fileName = e.target.value.split('\\').pop();
-			}
-			if (fileName) {
-				$label.html(fileName);
-				$(".btn").click(function() {
-					var imageUrl;
-					var formData = new FormData($("#upload")[0]);
-					$.ajax({
-						type: 'POST',
-						url: "https://sm.ms/api/upload",
-						data: formData,
-						processData: false, // tell jQuery not to process the data
-						contentType: false,
-						dataType: "json",
-						error: function() {
-							console.log("出现错误");
-						},
-						success: function(data) {
-							imageUrl = data.data.url;
-							$(".img").show();
-							$(".showUrl1").val(imageUrl);
-							$(".showUrl2").val('![](' + imageUrl + ')');
-							$(".img img").attr("src", imageUrl);
-						},
-					});
-					return false;
+
+
+	var $input = $('#smfile'),
+		$label = $input.next('label'),
+		labelVal = $label.html();
+	$input.on('change', function (e) {
+		$(".img").hide();
+		$(".tooltips").hide();
+		if (e.target.value) {
+			fileName = e.target.value.split('\\').pop();
+		}
+		if (fileName) {
+			$label.html(fileName);
+			$(".btn").click(function () {
+				var imageUrl;
+				var formData = new FormData($("#upload")[0]);
+				console.log(formData);
+				$.ajax({
+					type: 'POST',
+					url: "https://sm.ms/api/upload?ssl=true",
+					data: formData,
+					processData: false,
+					contentType: false,
+					dataType: "json",
+					error: function () {
+						console.log("出现错误");
+					},
+					success: function (data) {
+						imageUrl = data.data.url;
+						$(".img").show();
+						$(".showUrl1").val(imageUrl);
+						$(".showUrl2").val('![](' + imageUrl + ')');
+						$(".img img").attr("src", imageUrl);
+					},
 				});
-			} else {
-				$label.html(labelVal);
-			}
-		});
+				return false;
+			});
+		} else {
+			$label.html(labelVal);
+		}
 	});
-})
+});
